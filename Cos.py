dc0f46ccbb092f5e1012deef1b38b3d558e1d923
os-mari.kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2023-05-28 11:37:34 +0200

    Initial commit

---
+++ Cos.py
@@ -0,0 +1,36 @@
+from tkinter import*
+
+Window=Tk ()
+
+Window.title('ПР-14:Liana Kolodgeeva')
+
+Window.geometry ('600x400')
+
+Window.resizable (0,0)
+
+
+
+def fun1 (event):
+    Window['bg'] = 'Agua'
+    Lab = Label (Window, text='Ви натиснули на кнопку №1',   font='Times 20', fg='blue')
+    Lab.place ( x = 150 , y = 150 )
+
+def fun2 (event):
+
+    Window['bg']='yellow'
+    Lab = Label (Window, text='Ви натиснули на кнопку №2', font='Times 20', fg= 'green')
+    Lab.place ( x = 150 , y = 150)
+
+But1 = Button (Window, width=10,height=2, bg='violet', text='Kнoпка 1', fg='red', font='Times 12')
+
+But1.place ( x = 40 , y = 250)
+
+But1.bind('<Button-1>', fun1)
+
+But2 = Button (Window, width=10,height=2, bg='violet', text='Kнoпка 2', fg='red', font='Times 12')
+
+But2.place( x = 400 , y = 250 )
+
+But2.bind('<Button-1>', fun2)
+
+Window.mainloop()
